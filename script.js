343a5a732edb77bb339ec334a0956d26d558f173
Ashim Poudel <poudelashim26@gmail.com>
2022-09-12 18:01:02 +1000

    how passing arguments works:Values vs reference

--- script.js
+++ script.js
@@ -20,3 +20,32 @@ const createBooking = function (flightNum, numPassengers = 1, price = 199) {
 };
 createBooking("MH141");
 createBooking("MH141", 2, 800);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//How passing arguments works: Value vs Reference
+
+const flight = "MH141";
+const ashim = {
+  name: "Ashim Poudel",
+  passport: 123456,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = "MH123";
+  passenger.name = "Mr." + passenger.name;
+  if (passenger.passport === 123456) {
+    alert("check in");
+  } else {
+    alert("wrong passport");
+  }
+};
+// checkIn(flight, ashim);
+// console.log(flight);
+// console.log(ashim);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000);
+};
+newPassport(ashim);
+checkIn(flight, ashim);
